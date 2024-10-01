993a7fbf2717deb715970b0e7773a442428f95c3
aditya13raja <aditya13raja@gmail.com>
2024-10-01 14:26:20 +0530

    Create update listing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -14,16 +14,39 @@ export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
   if(!listing) {
-    next(errorHandler(404, 'Listing not found!'));
+    return next(errorHandler(404, 'Listing not found!'));
   }
 
   if(req.user.id !== listing.userRef) {
-    next(errorHandler(401, 'You can only delete your own listings!'));
+    return next(errorHandler(401, 'You can only delete your own listings!'));
   }
 
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json('Listing deleted successfully');
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if(!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+
+  if(req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only delete your own listings!'));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new: true},
+    );
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
