a798c7939a6e4c1ea642327138cb3355ccc55d20
aditya13raja <aditya13raja@gmail.com>
2025-01-08 21:55:36 +0530

    Remove a bug for deploying

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/listing', listingRoute);
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
